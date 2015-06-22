d40968965c76c22e66e15a9a6970d37371374164
Chance Snow <git@chancesnow.me>
2015-06-21 23:06:47 -0700

    Refine AboutDialog

--- Animatum/Dialogs/AboutDialog.Designer.cs
+++ Animatum/Dialogs/AboutDialog.Designer.cs
@@ -30,9 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutDialog));
             this.contentPanel = new System.Windows.Forms.Panel();
+            this.copyrightTextBox = new System.Windows.Forms.TextBox();
             this.versionLabel = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.copyrightTextBox = new System.Windows.Forms.TextBox();
             this.contentPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -46,21 +46,37 @@
             this.contentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.contentPanel.Location = new System.Drawing.Point(0, 0);
             this.contentPanel.Name = "contentPanel";
-            this.contentPanel.Size = new System.Drawing.Size(280, 230);
+            this.contentPanel.Size = new System.Drawing.Size(280, 250);
             this.contentPanel.TabIndex = 0;
             this.contentPanel.Click += new System.EventHandler(this.contentPanel_Click);
             // 
+            // copyrightTextBox
+            // 
+            this.copyrightTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.copyrightTextBox.Location = new System.Drawing.Point(11, 98);
+            this.copyrightTextBox.Multiline = true;
+            this.copyrightTextBox.Name = "copyrightTextBox";
+            this.copyrightTextBox.ReadOnly = true;
+            this.copyrightTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.copyrightTextBox.Size = new System.Drawing.Size(256, 139);
+            this.copyrightTextBox.TabIndex = 3;
+            this.copyrightTextBox.Text = resources.GetString("copyrightTextBox.Text");
+            // 
             // versionLabel
             // 
-            this.versionLabel.AutoSize = true;
-            this.versionLabel.Location = new System.Drawing.Point(93, 66);
+            this.versionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.versionLabel.Location = new System.Drawing.Point(11, 66);
             this.versionLabel.Name = "versionLabel";
-            this.versionLabel.Size = new System.Drawing.Size(93, 13);
+            this.versionLabel.Size = new System.Drawing.Size(256, 13);
             this.versionLabel.TabIndex = 1;
             this.versionLabel.Text = "Version 1.0 beta 1";
+            this.versionLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.pictureBox1.Image = global::Animatum.Properties.Resources.Logo190x51_trans;
             this.pictureBox1.Location = new System.Drawing.Point(44, 12);
             this.pictureBox1.Name = "pictureBox1";
@@ -69,25 +85,17 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // copyrightTextBox
-            // 
-            this.copyrightTextBox.Location = new System.Drawing.Point(11, 96);
-            this.copyrightTextBox.Multiline = true;
-            this.copyrightTextBox.Name = "copyrightTextBox";
-            this.copyrightTextBox.ReadOnly = true;
-            this.copyrightTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.copyrightTextBox.Size = new System.Drawing.Size(256, 121);
-            this.copyrightTextBox.TabIndex = 3;
-            this.copyrightTextBox.Text = resources.GetString("copyrightTextBox.Text");
-            // 
             // AboutDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(280, 230);
+            this.ClientSize = new System.Drawing.Size(280, 250);
             this.Controls.Add(this.contentPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "AboutDialog";
+            this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "About Animatum";
